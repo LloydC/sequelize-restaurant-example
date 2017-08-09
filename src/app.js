0c17a3243faaee64fc88aea7e2d5ba59d38eba7a
LloydC <lloydchambrier@gmail.com>
2017-08-09 08:47:22 +0200

    Created login page, added routes for logging in and signup

--- src/app.js
+++ src/app.js
@@ -62,3 +62,68 @@ Table.hasMany(Order)
 Table.belongsTo(Waiter)
 Order.belongsTo(Waiter)
 Order.belongsTo(Table)
+
+//----------------ROUTES----------------
+
+//ROUTE 01: HOME------------------------
+app.get('/', function(req, res){
+
+	var user = req.session.user
+
+	res.render("home", {user: user})
+
+});
+
+//CHECKING IF FORM INPUT USERDATA MATCHES DATABASE ENTRY. IF YES, ASSIGN SESSION TO USER.
+app.post('/', function (req, res) {
+
+  var name = req.body.name;
+	var password = req.body.password;
+
+	console.log('Just to make sure I get: '+username+" "+password);
+
+	if(name.length === 0) {
+		res.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
+		return;
+	}
+
+	if(password.length === 0) {
+		res.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
+		return;
+	}
+
+	Waiter.findOne({
+		where: {
+			name: name
+		}
+	}).then(function(waiter){
+
+			if(waiter!== null && password === waiter.password){
+        req.session.user = user;
+				res.redirect('/myprofile');
+			} else {
+				res.redirect('/?message=' + encodeURIComponent('Invalid email or password.'));
+			}
+	});
+});
+
+//ROUTE 02: CREATING NEW USER IN SIGNUP-------------
+app.get('/signup', function(req, res){
+	res.render("signup");
+})
+
+app.post('/signup', function(req, res){
+
+	var inputname = req.body.name
+  var inputemail = req.body.email
+	var inputpassword = req.body.password
+
+	console.log("I am receiving following waiter credentials: "+inputname+" "+inputpassword);
+
+			Waiter.create({
+				name: inputname,
+				password: hash
+			}).then( () => {
+				res.redirect('/?message=' + encodeURIComponent("Your waiter got successfully created. Log in below."));
+			});
+})
